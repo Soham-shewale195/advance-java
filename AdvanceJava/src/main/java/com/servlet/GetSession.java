f396ea52dbdd07879c130777dfa4ecbda365f7ca
Soham-shewale195 <shewalesoham195@gmail.com>
2025-06-21 15:18:25 +0530

    In Advance java update passward add feature add

--- AdvanceJava/src/main/java/com/servlet/GetSession.java
+++ AdvanceJava/src/main/java/com/servlet/GetSession.java
@@ -14,12 +14,13 @@ public class GetSession extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
 		PrintWriter out=res.getWriter();
 		HttpSession session=req.getSession();
 		String user=(String) session.getAttribute("user");
-		String pass=(String) session.getAttribute("pass");
+//		String pass=(String) session.getAttribute("pass");
 		out.print("Hello user :"+user);
-		out.print("<br>passward :"+pass);
+//		out.print("<br>passward :"+pass);
 		 out.print("<br>session id : "+session.getId());
 		 
 		 out.print("<form action = 'LogoutSevlet'>");
